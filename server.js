811373940a218b053d35b38a330e8ec89129f49a
Ifeanyi m. Nzekwue <i.nzekwe@lsnl.com>
2025-02-12 10:02:14 +0100

    Updated changes on different files

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.use('/api/auth', authRoutes);
 
 
 // Server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.DB_PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
