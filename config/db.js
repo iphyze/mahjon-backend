abd05906fc0fec20db81b1477a4273f29421b484
Ifeanyi m. Nzekwue <i.nzekwe@lsnl.com>
2025-02-12 01:15:35 +0100

    Updated db.js file

--- config/db.js
+++ config/db.js
@@ -1,21 +1,40 @@
-import mysql from 'mysql2';
+// import mysql from 'mysql2';
+// import dotenv from 'dotenv';
+
+// dotenv.config();
+
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME
+// });
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error('Database connection failed: ' + err.stack);
+//     return;
+//   }
+//   console.log('Connected to MySQL database');
+// });
+
+// export default db;
+
+
+import mysql from 'mysql2/promise';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_NAME
-});
-
-db.connect((err) => {
-  if (err) {
-    console.error('Database connection failed: ' + err.stack);
-    return;
-  }
-  console.log('Connected to MySQL database');
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 export default db;
