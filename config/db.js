82b2312de52fd42210cc8363861bb0079b8af91f
Ifeanyi m. Nzekwue <i.nzekwe@lsnl.com>
2025-02-13 11:10:20 +0100

    Updated db to use SSL

--- config/db.js
+++ config/db.js
@@ -8,6 +8,9 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
+  ssl: {
+    rejectUnauthorized: true,
+  },
   connectTimeout: 10000,
 });
 
