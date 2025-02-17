b30969c147a44e57283e39c145011d411e8dea2f
Ifeanyi m. Nzekwue <i.nzekwe@lsnl.com>
2025-02-17 10:58:38 +0100

    Fixed database to use pool

--- config/db.js
+++ config/db.js
@@ -3,23 +3,52 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const db = mysql.createConnection({
+// const db = mysql.createConnection({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASS,
+//   database: process.env.DB_NAME,
+//   // ssl: {
+//   //   rejectUnauthorized: true,
+//   // },
+//   connectTimeout: 10000,
+// });
+
+
+
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  // ssl: {
-  //   rejectUnauthorized: true,
-  // },
-  connectTimeout: 10000,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+  connectTimeout: 10000
 });
 
-db.connect((err) => {
+// Get a promise-based interface for the pool
+const db = pool.promise();
+
+
+// db.connect((err) => {
+//   if (err) {
+//     console.error('Database connection failed: ' + err.stack);
+//     return;
+//   }
+//   console.log('Connected to MySQL database');
+// });
+
+
+// Test the connection
+pool.getConnection((err, connection) => {
   if (err) {
     console.error('Database connection failed: ' + err.stack);
     return;
   }
   console.log('Connected to MySQL database');
+  connection.release();
 });
+
 
 export default db;
